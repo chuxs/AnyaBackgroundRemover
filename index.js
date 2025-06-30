c82354baa3040b3f5cce403db54828a6c6dffcde
chuxs <chuksanyaanya@gmail.com>
2025-06-30 15:05:23 +0100

    background remover

---
+++ index.js
@@ -0,0 +1,23 @@
+import express from "express";
+import bodyParser from "body-parser";
+import path from "path";
+import { fileURLToPath } from "url";
+import axios from "axios";
+
+const app = express();
+const port = process.env.PORT || 3000;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "Public")));
+
+app.get("/", (req, res) => {
+    res.render("index.ejs");
+  });
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
